fb983188ebf92201eefd3d430d143a823aed1f02
Conrad King <100320359+Reinboar@users.noreply.github.com>
2023-03-23 20:47:10 -0500

    Remove redundant quotes

--- examples/number_game/make.sh
+++ examples/number_game/make.sh
@@ -2,8 +2,7 @@
 filename="number"
 gbforth_loc="../../gbforth.rb"
 
-ruby $gbforth_loc $filename.ft -o "$filename".asm -I "../../" &&
-  rgbasm -l "$filename".asm -o "$filename".o -I "../../" &&
+ruby $gbforth_loc $filename.ft -o $filename.asm -I "../../" &&
+  rgbasm -l $filename.asm -o $filename.o -I "../../" &&
   rgblink $filename.o -o $filename.gb -n $filename.sym &&
   rgbfix -f lhg $filename.gb
-
