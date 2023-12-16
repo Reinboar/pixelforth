300798e3b165b472b56a94a1aecb52e550830381
Conrad <reinboarang@gmail.com>
2023-12-16 09:18:01 -0600

    Pad gameboy ROM when fixing header.

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ filename="$(dirname $1)/$(basename -s .ft $1)"
 ruby gbforth.rb $filename.ft -o "$filename".asm &&
   rgbasm -l "$filename".asm -o "$filename".o &&
   rgblink $filename.o -o $filename.gb -n $filename.sym &&
-  rgbfix -f lhg $filename.gb
+  rgbfix -f lhg -p0 $filename.gb
 
